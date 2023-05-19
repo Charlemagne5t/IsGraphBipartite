8bfd1774a16d2d267e318378ea8c45cf1b0dcb29
alek <wait82985@gmail.com>
2023-05-19 13:55:05 +0600

    77/81 tests passed. The issue is probably in testcases with disconnected graphs

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,50 @@
 package org.example;
 
+import java.util.*;
+
 public class Solution {
     public boolean isBipartite(int[][] graph) {
+        Set<Integer> visited = new HashSet<>();
+        boolean result = false;
+        Set<Integer> firstSet = new HashSet<>();
+        Set<Integer> secondSet = new HashSet<>();
+
+        Stack<Integer> nodes = new Stack<>();
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[i].length != 0) {
+                nodes.push(i);
+                break;
+            }
+        }
+
+        int switcher = 1;
+        while (!nodes.isEmpty()) {
+            int current = nodes.pop();
+            visited.add(current);
+            if (switcher == 1) {
+                firstSet.add(current);
+                for (int i = 0; i < graph[current].length; i++) {
+                    if (firstSet.contains(graph[current][i])) {
+                        return false;
+                    }
+                }
+                switcher *= -1;
+            } else if (switcher == -1) {
+                secondSet.add(current);
+                for (int i = 0; i < graph[current].length; i++) {
+                    if (secondSet.contains(graph[current][i])) {
+                        return false;
+                    }
+                }
+                switcher *= -1;
+            }
+            for (int i = 0; i < graph[current].length; i++) {
+                if (!visited.contains(graph[current][i])) {
+                    nodes.push(graph[current][i]);
+                    break;
+                }
+            }
+        }
         return true;
     }
 
