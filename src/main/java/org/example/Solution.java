9297df32525c8d8506d6c3ba7de6e863953ada61
alek <wait82985@gmail.com>
2023-05-19 16:34:11 +0600

    problem solved

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,47 +5,45 @@ import java.util.*;
 public class Solution {
     public boolean isBipartite(int[][] graph) {
         Set<Integer> visited = new HashSet<>();
-        boolean result = false;
-        Set<Integer> firstSet = new HashSet<>();
-        Set<Integer> secondSet = new HashSet<>();
+
 
         Stack<Integer> nodes = new Stack<>();
         for (int i = 0; i < graph.length; i++) {
-            if (graph[i].length != 0) {
+            if (graph[i].length != 0 && !visited.contains(i)) {
                 nodes.push(i);
-                break;
+                Set<Integer> firstSet = new HashSet<>();
+                Set<Integer> secondSet = new HashSet<>();
+                int switcher = 1;
+                while (!nodes.isEmpty()) {
+                    int current = nodes.pop();
+                    visited.add(current);
+                    if (switcher == 1) {
+                        firstSet.add(current);
+                        for (int j = 0; j < graph[current].length; j++) {
+                            if (firstSet.contains(graph[current][j])) {
+                                return false;
+                            }
+                        }
+                        switcher *= -1;
+                    } else if (switcher == -1) {
+                        secondSet.add(current);
+                        for (int j = 0; i < graph[current].length; i++) {
+                            if (secondSet.contains(graph[current][i])) {
+                                return false;
+                            }
+                        }
+                        switcher *= -1;
+                    }
+                    for (int j = 0; j < graph[current].length; j++) {
+                        if (!visited.contains(graph[current][j])) {
+                            nodes.push(graph[current][j]);
+                        }
+                    }
+                }
             }
         }
 
-        int switcher = 1;
-        while (!nodes.isEmpty()) {
-            int current = nodes.pop();
-            visited.add(current);
-            if (switcher == 1) {
-                firstSet.add(current);
-                for (int i = 0; i < graph[current].length; i++) {
-                    if (firstSet.contains(graph[current][i])) {
-                        return false;
-                    }
-                }
-                switcher *= -1;
-            } else if (switcher == -1) {
-                secondSet.add(current);
-                for (int i = 0; i < graph[current].length; i++) {
-                    if (secondSet.contains(graph[current][i])) {
-                        return false;
-                    }
-                }
-                switcher *= -1;
-            }
-            for (int i = 0; i < graph[current].length; i++) {
-                if (!visited.contains(graph[current][i])) {
-                    nodes.push(graph[current][i]);
-                    break;
-                }
-            }
-        }
+
         return true;
     }
-
 }
